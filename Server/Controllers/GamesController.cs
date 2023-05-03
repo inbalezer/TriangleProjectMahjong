5d9904fbbcfc2882348263f81d02fcfc17afe2ec
Malkaberry <84181559+Malkaberry@users.noreply.github.com>
2023-05-03 16:31:28 +0300

    newGameController
    needed to be checked with the database

--- Server/Controllers/GamesController.cs
+++ Server/Controllers/GamesController.cs
@@ -49,5 +49,70 @@ namespace TriangleProject.Server.Controllers
             }
             return BadRequest("No Session");
         }
+
+
+        [HttpGet("addGame/{gameName}")]
+        public async Task<IActionResult> AddGames(int userId, string gameName)
+        {
+            int? sessionId = HttpContext.Session.GetInt32("userId");
+            if (sessionId != null)
+            {
+                if (userId == sessionId)
+                {
+                    object param = new
+                    {
+                        UserId = userId
+                    };
+                    string userQuery = "SELECT FirstName FROM Users WHERE ID = @UserId";
+                    var userRecords = await _db.GetRecordsAsync<UserWithGames>(userQuery, param);
+                    UserWithGames user = userRecords.FirstOrDefault();
+                    if (user != null)
+                    {
+                        object newGameParam = new
+                        {
+                            GameFullName = gameName,
+                            GameCode = 0,
+                            PublishStatus = "Not Eligible",
+                            TimePertelm = 60,
+                            // todo need to check if exists in the game table//
+                            UserId = userId
+
+                        };
+                        string insertGameQuery = "INSERT INTO Games (GameFullName, GameCode, PublishStatus, TimePerItem, UserId) " +
+                                "VALUES (@GameFullName, @GameCode, @PublishStatus, @TimePertelm, @UserId)";
+                        int newGameId = await _db.InsertReturnId(insertGameQuery, newGameParam);
+                        if (newGameId != 0)
+                        {
+                            int gameCode = newGameId + 100;
+                            object updateParam = new
+                            {
+                                ID = newGameId,
+                                GameCode = gameCode
+
+                            };
+                            string updateCodeQuery = "UPDATE Games SET GameCode = @GameCode	WHERE ID=@ID";
+                            bool isUpdate = await _db.SaveDataAsync(updateCodeQuery, updateParam);
+                            if (isUpdate == true)
+                            {
+                                object param2 = new
+                                {
+                                    ID = newGameId
+                                };
+                                string gameQuery = "SELECT ID,GameFullName,GameCode,PublishStatus FROM Games WHERE ID = @ID";
+                                var gameRecord = await _db.GetRecordsAsync<Game>(gameQuery, param2);
+                                Game newGame = gameRecord.FirstOrDefault();
+                                return Ok(newGame);
+                            }
+
+                        }
+                        return BadRequest("Game not created");
+                    }
+                    return BadRequest("User Not Found");
+                }
+                return BadRequest("User Not Logged In");
+            }
+            return BadRequest("No Session");
+        }
+
     }
 }

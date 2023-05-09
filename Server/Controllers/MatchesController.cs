813786b0e4f740cb2237e498eec3e73308720820
inbalezer <inbalezer2@gmail.com>
2023-05-09 15:26:00 +0300

    Update match method
    שיטה המעדכנת זוג במשחק ספציפי

--- Server/Controllers/MatchesController.cs
+++ Server/Controllers/MatchesController.cs
@@ -41,13 +41,27 @@ namespace TriangleProject.Server.Controllers
             return BadRequest("No Session");
         }
 
-        
-        
+        [HttpPost("EditMatch")]
 
+        public async Task<IActionResult> EditMatch(int userId, MatchToUpdate matchToUpdate)
+        {
+            int? sessionId = HttpContext.Session.GetInt32("userId");
+            if (sessionId != null)
+            {
+                if (userId == sessionId)
+                {
+                    string UpdateMatchQuery = "UPDATE Matches set FirstMatch = @FirstMatch, SecondMatch = @SecondMatch, FirstIsText =@FirstIsText, SecondIsText =@SecondIsText where ID=@ID AND GameID = @GameID";
+                    bool isUpdate = await _db.SaveDataAsync(UpdateMatchQuery, matchToUpdate);
 
-
-
-
-
+                    if (isUpdate)
+                    {
+                        return Ok(matchToUpdate);
+                    }
+                    return BadRequest("update match faild");
+                }
+                return BadRequest("User Not Logged In");
+            }
+            return BadRequest("No Session");
+        }
     }
 }
---
+++ Shared/Models/Editor/MatchToUpdate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleProject.Shared.Models.Editor
+{
+    public class MatchToUpdate
+    {
+        public int ID { get; set; }
+        public int GameID { get; set; }
+        public string FirstMatch { get; set; }
+        public string SecondMatch { get; set; }
+        public bool FirstIsText { get; set; }
+        public bool SecondIsText { get; set; }
+    }
+}

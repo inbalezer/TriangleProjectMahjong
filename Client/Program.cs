d0342b9511be59a28218ebc67826872612064930
Malkaberry <84181559+Malkaberry@users.noreply.github.com>
2023-06-29 20:00:33 +0300

    red input--fix

--- Client/Program.cs
+++ Client/Program.cs
@@ -10,7 +10,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<Portelem.Auth.AuthHelper>();
-builder.Services.AddMudServices();
+//builder.Services.AddMudServices();
 
 
 await builder.Build().RunAsync();
